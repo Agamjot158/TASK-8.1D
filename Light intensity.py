b7aa042b4eac70b509348df404c7e89e7c91ff36
Agamjot158 <106953882+Agamjot158@users.noreply.github.com>
2022-11-19 11:24:20 +0530

    Update Light intensity.py

--- Light intensity.py
+++ Light intensity.py
@@ -1,16 +1,20 @@
-import smbus
-import time
+import smbus              # Library used for I2C communication
+import time               # Time library used for adding delay function
 
-LIGHT_SENSOR = 0X23
+# Here we have set the address for the sensor
+LIGHT_SENSOR = 0X23       
 
 VALUE = 0X23
 
-bus=smbus.SMBus(1)
+bus=smbus.SMBus(1)     # Here we have set the I2C port to 1 
+
+# Here we have defined the function "reading", the data from the sensor is processed using this 
 
 def reading():
     data = bus.read_i2c_block_data(LIGHT_SENSOR,VALUE)
-    return ((data[1] + (256*data[0]))/1.2)
+    return ((data[1] + (256*data[0]))/1.2)                # Converting the reading into decimal format
 
+# Here we have made a loop that will print the messages and the readings from the sensor according to the parametes
 
 while True:
     light=reading()
